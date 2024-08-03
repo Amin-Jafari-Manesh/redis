cadcf603ecddd0ffbc34627dfe2a9ae291598615
Amin Jafari <amincorleone81@gmail.com>
2024-08-03 14:48:09 +0330

    initial

---
+++ redis_writer.py
@@ -0,0 +1,44 @@
+import logging
+import redis
+from os import environ
+
+db_config = {
+    'PASS': environ.get('PASS', ''),
+    'DOMAIN': environ.get('DOMAIN', ''),
+    'HASH_SIZE': environ.get('HASH_SIZE', ''),
+    'RECORDS': environ.get('RECORDS', ''),
+}
+
+
+def generate_random_hash(numb: int = 1) -> str:
+    import random
+    import string
+    import hashlib
+    if numb == 1:
+        return hashlib.sha256(''.join(random.choices(string.ascii_letters + string.digits, k=64)).encode()).hexdigest()
+    else:
+        return ''.join(
+            [hashlib.sha256(''.join(random.choices(string.ascii_letters + string.digits, k=64)).encode()).hexdigest()
+             for _ in range(numb)])
+
+
+def check_redis_connection():
+    try:
+        redis_client = redis.StrictRedis(host=db_config['DOMAIN'], port=6379,
+                                         password=db_config['PASS'])
+        redis_client.ping()
+        logging.info("Successfully connected to Redis")
+        return True
+    except redis.ConnectionError as e:
+        logging.error(f"Failed to connect to Redis: {str(e)}")
+        return False
+
+
+def redis_write_hash(size: int = 100) -> bool:
+    if check_redis_connection():
+        r = redis.StrictRedis(host=db_config['DOMAIN'], port=6379,
+                              password=db_config['PASS'])
+        for _ in range(size):
+            r.set(generate_random_hash(db_config['HASH_SIZE']), generate_random_hash(db_config['HASH_SIZE']))
+        return True
+    return False
