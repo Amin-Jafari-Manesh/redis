5ed2f8fb83cb8b8d2a9e1abfaa0d728e20cff474
Amin Jafari <amincorleone81@gmail.com>
2024-08-07 13:41:56 +0330

    initial

--- redis_writer.py
+++ redis_writer.py
@@ -1,50 +1,64 @@
 import logging
 import redis
 from os import environ
+import time
 
 logging.basicConfig(level=logging.INFO)
 
 db_config = {
     'PASS': environ.get('PASS', ''),
     'DOMAIN': environ.get('DOMAIN', ''),
-    'HASH_SIZE': int(environ.get('HASH_SIZE', '')),
+    'DATA_TYPE': environ.get('DATA_TYPE', ''),
+    'DATA_SIZE': int(environ.get('HASH_SIZE', '')),
     'RECORDS': int(environ.get('RECORDS', '')),
+    'INSERT_DELAY': int(environ.get('INSERT_DELAY', '')),
 }
 
+redis_client = redis.StrictRedis(host=db_config['DOMAIN'], port=6379,
+                                 password=db_config['PASS'])
 
-def generate_random_hash(numb: int = 1) -> str:
+
+def generate_random_hash(numb: int) -> str:
     import random
     import string
     import hashlib
-    if numb == 1:
-        return hashlib.sha256(''.join(random.choices(string.ascii_letters + string.digits, k=64)).encode()).hexdigest()
+    return ''.join(
+        [hashlib.sha256(''.join(random.choices(string.ascii_letters + string.digits, k=64)).encode()).hexdigest()
+         for _ in range(numb)])
+
+
+def generate_text(numb: int) -> str:
+    text = ' The quick brown fox jumps over the lazy dog today. '
+    return ''.join([text for _ in range(numb)])
+
+
+def redis_write(size: int = 100) -> bool:
+    if not redis_client.ping():
+        logging.error("Failed to connect to the database.")
+        return False
+
+    logging.info("Successfully connected to the database.")
+
+    if db_config['DATA_TYPE'] == 'h':
+        func = generate_random_hash
+    elif db_config['DATA_TYPE'] == 't':
+        func = generate_text
     else:
-        return ''.join(
-            [hashlib.sha256(''.join(random.choices(string.ascii_letters + string.digits, k=64)).encode()).hexdigest()
-             for _ in range(numb)])
+        logging.error("Invalid data type.")
+        return False
 
-
-def check_redis_connection():
     try:
-        redis_client = redis.StrictRedis(host=db_config['DOMAIN'], port=6379,
-                                         password=db_config['PASS'])
-        redis_client.ping()
-        logging.info("Successfully connected to Redis")
-        return True
-    except redis.ConnectionError as e:
-        logging.error(f"Failed to connect to Redis: {str(e)}")
+        for _ in range(size):
+            time.sleep(db_config['INSERT_DELAY'] * 0.001)
+            redis_client.set(func(db_config['DATA_SIZE']),
+                             func(db_config['DATA_SIZE']))
+    except Exception as e:
+        logging.error(f"Error writing to the database: {e}")
         return False
 
 
-def redis_write_hash(size: int = 100) -> bool:
-    if check_redis_connection():
-        r = redis.StrictRedis(host=db_config['DOMAIN'], port=6379,
-                              password=db_config['PASS'])
-        for _ in range(size):
-            r.set(generate_random_hash(db_config['HASH_SIZE']), generate_random_hash(db_config['HASH_SIZE']))
-        return True
-    return False
-
-
 if __name__ == '__main__':
-    redis_write_hash(db_config['RECORDS'])
+    if redis_write(db_config['RECORDS']):
+        logging.info("Data has been written successfully.")
+    else:
+        logging.error("Failed to write data.")
